4ba3c10e49e9f48fe9a3ef1770391edf14444cbd
LMunday98 <luke.munday@gmil.com>
2020-12-21 18:33:30 +0000

    Refactored query and table echo into sep functions inside index.php.

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <?php
 include 'config.php';
 
-$conn = getConnection();
-
-
-
-$sql = "SELECT * FROM Users";
-$result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
-echo "<br>";
-echo "<table border='1'>";
-while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
-    echo "<tr>";
-    foreach ($row as $field => $value) { // I you want you can right this line like this: foreach($row as $value) {
-        echo "<td>" . $value . "</td>"; // I just did not use "htmlspecialchars()" function.
-    }
-    echo "</tr>";
+function echoTable($table) {
+  $result = queryTable($table);
+  echo "<br>";
+  echo "<table border='1'>";
+  while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
+      echo "<tr>";
+      foreach ($row as $field => $value) { // I you want you can right this line like this: foreach($row as $value) {
+          echo "<td>" . $value . "</td>"; // I just did not use "htmlspecialchars()" function.
+      }
+      echo "</tr>";
+  }
+  echo "</table>";
 }
-echo "</table>";
+
+function queryTable($table) {
+  $conn = getConnection();
+  $sql = "SELECT * FROM " . $table;
+  $result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
+  return $result;
+}
+
+echoTable("Users");
 ?>
