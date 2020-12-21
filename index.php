6dc2fa64f6a3c9063bea9333acb0b5a9186acfa5
LMunday98 <luke.munday@gmil.com>
2020-12-21 18:06:01 +0000

    Now correctly remotes into external mysql server and echos all table data

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
-$mysqli = new mysqli("localhost","admin","371268","3CB105");
+//$mysqli = new mysqli("<IP of Server A>", "user", "password", "database");
+$conn = new mysqli("192.168.0.185:3306", "test", "", "3CB105");
+
+
+
 
 // Check connection
-if ($mysqli -> connect_errno) {
+if ($conn -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
+
+$sql = "SELECT * FROM Users";
+$result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
+echo "<br>";
+echo "<table border='1'>";
+while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
+    echo "<tr>";
+    foreach ($row as $field => $value) { // I you want you can right this line like this: foreach($row as $value) {
+        echo "<td>" . $value . "</td>"; // I just did not use "htmlspecialchars()" function.
+    }
+    echo "</tr>";
+}
+echo "</table>";
 ?>
